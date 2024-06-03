0461d09545a659e5456f778f3257373834c3b0cf
Ubuntu <ubuntu@ip-172-31-81-234.ec2.internal>
2024-06-03 13:25:22 +0000

    update istio install script

--- eks-client/istio-helm.sh
+++ eks-client/istio-helm.sh
@@ -2,4 +2,10 @@ helm repo add istio https://istio-release.storage.googleapis.com/charts
 helm repo update
 
 kubectl create namespace istio-ingress
-helm install istio-ingress istio/gateway -n istio-ingress --wait
+helm install istio-ingress istio/gateway -n istio-ingress \
+  --set labels.istio=ingressgateway \
+  --set service.annotations."service\.beta\.kubernetes\.io/aws-load-balancer-type"="external" \
+  --set service.annotations."service\.beta\.kubernetes\.io/aws-load-balancer-nlb-target-type"="ip" \
+  --set service.annotations."service\.beta\.kubernetes\.io/aws-load-balancer-scheme"="internet-facing" \
+  --set service.annotations."service\.beta\.kubernetes\.io/aws-load-balancer-attributes"="load_balancing.cross_zone.enabled=true" \
+  --wait
